537fc30bd7ce7389182fd7dd3cedd6bff52b09b8
Sharongoldie <goldiezulu97@gmail.com>
2023-09-01 15:07:51 -0700

    libmy.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - Prints a string to the standard output (stdout).
+ * @str: The string to be printed.
+ *
+ * Return: void.
+ */
+void _puts(char *str)
+{
+	/* Example: Printing the string */
+	printf("%s\n", str);
+}
